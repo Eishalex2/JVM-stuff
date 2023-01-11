dcde09c01d24e8f963bd14b7f9916a88620949cc
Eishalex2 <t.whitney.a@gmail.com>
2023-01-11 12:13:10 -0800

    feat: Filter out keywords from Austin MLS .xlsx
    Filter out keywords in the keywords list. Convert the finalized lists to
    dataframes and export to an excel file with two sheets. One sheet for
    entries to keep, one for the entries that have been filtered out.
    
    Next: slicing names

---
+++ filter-out.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+df = pd.read_excel('MLS-Austin-listings.xlsx')
+
+header = df.columns.tolist()
+
+df_list = df.values.tolist()
+keywords = ["cash only", "hard money only", "55+", "senior community", "no financing", "no loan", "commercial"]
+filtered = []
+bad = []
+
+for list in df_list:
+  if type(list[22]) == float:
+    filtered.append(list)
+  elif any(keyword in list[22].lower() for keyword in keywords):
+    bad.append(list)
+  else:
+    filtered.append(list)
+
+filtered_df = pd.DataFrame(filtered, columns=header)
+bad_df = pd.DataFrame(bad, columns=header)
+
+with pd.ExcelWriter("Austin.xlsx") as writer:
+  filtered_df.to_excel(writer, sheet_name="keep_austin")
+  bad_df.to_excel(writer, sheet_name="filt-out_austin")
