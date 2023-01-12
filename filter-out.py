296f2d55e7379fd38c1783c180450102cded5cd7
Eishalex2 <t.whitney.a@gmail.com>
2023-01-11 17:02:16 -0800

    feat: split full names into first, last
    Austin completed.

--- filter-out.py
+++ filter-out.py
@@ -1,8 +1,12 @@
 import pandas as pd
+import re
 
-df = pd.read_excel('MLS-Austin-listings.xlsx')
+df = pd.read_excel('MLS Austin listings_DE-DUPED.xlsx')
 
 header = df.columns.tolist()
+header[1] = "List Agent First Name"
+""" header.insert(2, "List Agent Last Name")
+print(header) """
 
 df_list = df.values.tolist()
 keywords = ["cash only", "cash offers", "hard money", "55+", "senior community", "no financing", "no loan", "commercial", "construction loan"]
@@ -17,7 +21,29 @@ for list in df_list:
   else:
     filtered.append(list)
 
+names = []
+for list in filtered:
+  split_name = list[1].split()
+  names.append(split_name)
+
+for nameset in names:
+  middle_last = nameset[1]
+  if len(middle_last) == 1 or middle_last[1] == ".":
+    nameset.pop(1)
+  if len(nameset) > 2:
+    nameset[1] = " ".join(nameset[1:])
+    del nameset[2:]
+
+first_name = []
+last_name = []
+for name in names:
+  first_name.append(name[0])
+  last_name.append(name[1])
+
 filtered_df = pd.DataFrame(filtered, columns=header)
+filtered_df["List Agent First Name"] = first_name
+filtered_df.insert(2, "List Agent Last Name", last_name)
+
 bad_df = pd.DataFrame(bad, columns=header)
 
 with pd.ExcelWriter("Austin.xlsx") as writer:
