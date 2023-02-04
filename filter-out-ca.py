68440c8153e7b233a744e2cdf31a92510fe2d356
Eishalex2 <t.whitney.a@gmail.com>
2023-02-03 17:31:33 -0800

    feat: Make agent and addresses title changes
    Also, fixed situation where listing agent does not have a middle or last
    name by creating blank last name entries, to keep the index numbers
    consistent.
    
    Address title case seems to fail for some entries. Need to look into that.

--- filter-out-ca.py
+++ filter-out-ca.py
@@ -1,44 +1,54 @@
 import pandas as pd
 
-df = pd.read_excel('../CA MLS A-Y.xlsx')
+df = pd.read_excel('../Listing_Agent_Stale.xlsx')
 
 header = df.columns.tolist()
 header[0] = "List Agent First Name"
 
 df_list = df.values.tolist()
-keywords = ["cash only", "cash offer", "cash buyer", "cash sale", "cash transaction only", "hard money", "55+", "senior community", "no financing", "no loan", "commercial", "construction loan"]
+#list of keywords to filter out into the 'bad' worksheet
+keywords = ["time share", "cash only", "cash offer", "cash buyer", "cash sale", "cash transaction only", "hard money", "55+", "senior community", "no financing", "no loan", "commercial", "construction loan"]
 keep = []
 bad = []
 
 
-#Number is for Confidential remarks. Assuming column C
+#Number is for Confidential remarks. Assuming column E
+#Filtering out the above keywords
 for list in df_list:
-  if type(list[2]) == float:
+  if type(list[4]) == float:
     keep.append(list)
-  elif any(keyword in list[2].lower() for keyword in keywords):
+  elif any(keyword in list[4].lower() for keyword in keywords):
     bad.append(list)
   else:
     keep.append(list)
+  list[2].lower().title()
 
-#Assuming names in column Agit 
+#Assuming names in column A
+#Splitting listing agent names 
 names = []
 for list in keep:
   split_name = list[0].split()
   names.append(split_name)
 
+#Removing middle initials and joining all full middle and last names
+#into one column
 for nameset in names:
-  middle_last = nameset[1]
-  if len(middle_last) == 1 or middle_last[1] == ".":
-    nameset.pop(1)
-  if len(nameset) > 2:
-    nameset[1] = " ".join(nameset[1:])
-    del nameset[2:]
+  if len(nameset) > 1:
+    middle_or_last = nameset[1]
+    if len(middle_or_last) == 1 or middle_or_last[1] == ".":
+      nameset.pop(1)
+    if len(nameset) > 2:
+      nameset[1] = " ".join(nameset[1:])
+      del nameset[2:]
 
 first_name = []
 last_name = []
 for name in names:
   first_name.append(name[0].title())
-  last_name.append(name[1].title())
+  if len(name) > 1:
+    last_name.append(name[1].title())
+  else:
+    last_name.append('')
 
 keep_df = pd.DataFrame(keep, columns=header)
 keep_df["List Agent First Name"] = first_name
